05935559b8c56cbca91800b2465fe9f341c36901
mindaugas <mindaugas9112@gmail.com>
2024-02-26 22:29:44 +0200

    feat: TransferFunction working accurate

--- common_defs.hpp
+++ common_defs.hpp
@@ -2,12 +2,39 @@
 
 #include "ap_fixed.h"
 #include <complex>
+/* ================================================================= */
+/* DEFINES */
+/* ================================================================= */
+#define DEBUG
 
 #define ARGS_SIZE 6
 #define PARAMS_SIZE 3
 #define FREQ_AXIS_SIZE 2048
 #define TRANSFER_FUNC_SIZE 2048
 
+#define TEST_DIR "D:/Magistras/Magistro projektas/Matlab_realization/Raw/test/TransferFunction/"
+#define ARGS_DIR TEST_DIR "OriginalArgs.txt"
+#define PARAMS_DIR TEST_DIR "acquisition_parameters.txt"
+#define FREQ_AXIS_DIR TEST_DIR "freq_axis.txt"
+#define TRANSFER_FUN_RE_DIR TEST_DIR "T_re.txt"
+#define TRANSFER_FUN_IM_DIR TEST_DIR "T_im.txt"
+/* ================================================================= */
+/* MACROS */
+/* ================================================================= */
+
+#ifdef DEBUG
+#define PRINT(variable, i) printf(#variable "[%d]: %f\n", i, (double)variable);
+#define PRINT_C(variable, i)                                                   \
+    printf(#variable "[%d]: %f, %f i\n", i, (double)variable.real(),             \
+           (double)variable.imag());
+#else
+#define PRINT(variable, i)
+#define PRINT_C(variable, i)
+#endif
+
+/* ================================================================= */
+/* TYPEDEFS */
+/* ================================================================= */
 typedef ap_fixed<32, 16> fixed_float;
 typedef ap_fixed<48, 32> fixed_big_float;
 typedef ap_fixed<64, 48> fixed_big_big_float;
@@ -32,10 +59,3 @@ struct sModelParams {
     fixed_float c1;
     fixed_float ro1;
 };
-
-#define TEST_DIR "D:/Magistras/Magistro projektas/Matlab_realization/Raw/test/TransferFunction/"
-#define ARGS_DIR TEST_DIR "OriginalArgs.txt"
-#define PARAMS_DIR TEST_DIR "acquisition_parameters.txt"
-#define FREQ_AXIS_DIR TEST_DIR "freq_axis.txt"
-#define TRANSFER_FUN_RE_DIR TEST_DIR "T_re.txt"
-#define TRANSFER_FUN_IM_DIR TEST_DIR "T_im.txt"
--- model.cpp
+++ model.cpp
@@ -7,8 +7,8 @@ static const fixed_double PI = 3.14159265358979323846;
 void TransferFunction(const sModelArgs args, const sModelParams params,
                       const fixed_double *freq_axis, complex_float *tf_out) {
     
-    fixed_big_big_float decimation = 1000000;
-    complex_big_big_float imag_unit = complex_big_big_float(0, 1);
+    fixed_double decimation = 1e9;
+    complex_double imag_unit = complex_double(0, 1);
 
     fixed_double alf = 0;
     fixed_double alf_1 = 0;
@@ -18,82 +18,80 @@ void TransferFunction(const sModelArgs args, const sModelParams params,
     fixed_double k_real = 0;
     fixed_double k_imag = 0;
 
-    fixed_big_big_float Z1 = params.c1 * params.ro1;
-    fixed_big_big_float Z2 = args.c2 * args.ro2;
+    fixed_double Z1 = params.c1 * params.ro1;
+    fixed_double Z1_dec = Z1 / decimation;
+    fixed_double Z2 = args.c2 * args.ro2;
+    fixed_double Z2_dec = Z2 / decimation;
+    PRINT(Z1, 0);
+    PRINT(Z2, 0);
 
-    fixed_big_big_float numerator = 2 * Z1 * Z2;
-    fixed_big_big_float denominator_sum_sqr = Z1 * Z1 + Z2 * Z2;
-    complex_big_big_float denominator;
-    complex_big_big_float T;
-    
+    fixed_double numerator = 2 * Z1 * Z2_dec; // DECIMATED!
+    PRINT(numerator, 0);
 
-    fixed_float real_kh = 0;
-    fixed_float imag_kh = 0;
+    fixed_double denominator_sum_sqr = Z1 * Z1_dec + Z2 * Z2_dec; // DECIMATED!
+    PRINT(denominator_sum_sqr, 0);
 
-    fixed_float cos_kh_real = 0;
-    fixed_float cos_kh_imag = 0;
-    complex_big_big_float cos_kh;
+    fixed_double real_kh = 0;
+    fixed_double imag_kh = 0;
 
-    fixed_float sin_kh_real = 0;
-    fixed_float sin_kh_imag = 0;
-    complex_big_big_float sin_kh;
+    fixed_double cos_kh_real = 0;
+    fixed_double cos_kh_imag = 0;
+    complex_double cos_kh;
 
-    printf("Z1: %f, Z2: %f, numerator: %f\n", (double)Z1, (double)Z2,
-           (double)numerator);
+    fixed_double sin_kh_real = 0;
+    fixed_double sin_kh_imag = 0;
+    complex_double sin_kh;
+
+    complex_double denominator;
+    complex_double T;
 
 
     tf_out[0] = 0;
     for (int i = 1; i < TRANSFER_FUNC_SIZE; i++) {
         /* Calculate alfa - attenuation coefficient */
         alf_1 = hls::abs(freq_axis[i]) / args.freq0;
+        // PRINT(alf_1, i);
         alf_2 = hls::pow((float)alf_1, (float)args.n);
+        // PRINT(alf_2, i);
         alf = args.alfa0 * alf_2;
-        // printf("alf_1: %f, alf_2: %f, alf: %f\n", (float)alf_1, (float)alf_2,
-        // (float)alf);
+        // PRINT(alf, i);
 
         /* Calculate k - wave number */
+        // PRINT(freq_axis[i], i);
         k_real = 2 * PI * freq_axis[i] / args.c2;
         k_imag = alf;
-        // printf("freq_axis[%d]: %f, k_real: %f, k_imag: %f\n", i,
-        //        (double)freq_axis[i], (double)k_real, (double)k_imag);
+        // PRINT(k_real, i);
+        // PRINT(k_imag, i);
 
         /* Calculate transfer function */
+        /* ================================================================= */
         /* Brekhovskikh & Godin 1990 (2.4.18) */
         /* T =  (2 * Z1 * Z2) ./ 
                 ((2 * Z1 * Z2) .* cos(k.*h) - 1i .* sin(k.*h) .* (Z1.^2 + Z2.^2)); */
+        /* ================================================================= */
 
         real_kh = k_real * args.h;
         imag_kh = k_imag * args.h;
-        // printf("kh[%d]: %f, %f\n", i, (double)real_kh, (double)imag_kh);
+        // PRINT(real_kh, i);
+        // PRINT(imag_kh, i);
 
         /* Apply Euler's Cosine Identity replacement */
         cos_kh_real = hls::cos(real_kh) * hls::cosh(imag_kh);
         cos_kh_imag = -1 * hls::sin(real_kh) * hls::sinh(imag_kh);
         cos_kh = complex_float(cos_kh_real, cos_kh_imag);
-        // printf("cos_kh[%d]: %f, %f\n", i, (double)cos_kh.real(), (double)cos_kh.imag());
+        PRINT_C(cos_kh, i);
 
         /* Apply Euler's Sine Identity replacement */
         sin_kh_real = hls::sin(real_kh) * hls::cosh(imag_kh);
         sin_kh_imag = hls::cos(real_kh) * hls::sinh(imag_kh);
         sin_kh = complex_float(sin_kh_real, sin_kh_imag);
-        // printf("sin_kh[%d]: %f, %f\n", i, (double)sin_kh.real(), (double)sin_kh.imag());
+        PRINT_C(sin_kh, i);
 
-        denominator = (numerator * cos_kh) - (imag_unit * sin_kh * denominator_sum_sqr);
-        printf("denominator[%d]: %f, %f\n", i, (double)denominator.real(), (double)denominator.imag());
-        denominator = denominator / decimation;
-        // printf("%f, %f\n", (double)(imag_unit * sin_kh).real(), (double)(imag_unit * sin_kh).imag());
-        // printf("%e, %e\n", (double)denominator.real(), (double)denominator.imag());
+        denominator = (numerator * cos_kh) - (imag_unit * sin_kh * denominator_sum_sqr); // DECIMATED!
+        PRINT_C(denominator, i);
 
         T = numerator / denominator;
-        T = T / decimation;
-        tf_out[i] = complex_float(T.real(), T.imag());
-        printf("T[%d]: %f, %f\n", i, (double)T.real(), (double)T.imag());
-        // printf("%e / %e %ei = %e %ei\n", (double)numerator,
-        //        (double)denominator.real(), (double)denominator.imag(),
-        //        (double)tf_out[i].real(), (double)tf_out[i].imag());
-        // printf("%f / %f = %f\n", (double)numerator.imag(),
-        // (double)denominator.imag(), (double)tf_out[i].imag());
-        // printf("tf_out[%d]: %f, %f\n", i, (double)tf_out[i].real(),
-        // (double)tf_out[i].imag());
+        PRINT_C(T, i);
+        tf_out[i] = T;
     }
 }
